61bdfe1a9007de4368bcf343f169410c7cc0ad3f
Itshnic72 <rv920490@gmail.com>
2024-10-08 14:08:10 +0500

    less_6

--- lessons.js
+++ lessons.js
@@ -10,14 +10,14 @@ let admin = name;
 console.log("Администратор", admin);
 
 console.log("Less - 2");
-let num1 = parseFloat(prompt("Введите значение для num1:"));
+/* let num1 = parseFloat(prompt("Введите значение для num1:"));
 let num2 = parseFloat(prompt("Введите значение для num2:"));
 
 if (num1 <= 1 && num2 >= 3) {
 	console.log("Условия выполнены");
 } else {
 	console.log("Условия не выполнены");
-}
+} */
 
 let test = true;
 console.log(test == true ? "+++" : "---");
@@ -26,3 +26,49 @@ let day = 31;
 
 let period = day <= 10 ? "первая" : day > 10 && day <= 20 ? "вторая" : "третья";
 console.log(period);
+
+console.log("Less - 6");
+
+function pow(a) {
+	return a ** 3;
+}
+console.log(pow(3));
+
+function ndfl(summ) {
+	if (isNaN(summ)) return "Значение введено не верно!";
+	else
+		return `Размер заработной платы за вычетом налогов равен:${
+			summ - summ * 0.13
+		}р.`;
+}
+console.log(ndfl(100000));
+console.log(ndfl("Привет"));
+
+function max_number() {
+	let num_1 = parseFloat(prompt("Введите первое число:"));
+	let num_2 = parseFloat(prompt("Введите второе число:"));
+	let num_3 = parseFloat(prompt("Введите третье число:"));
+
+	return num_1 > num_2 && num_1 > num_3
+		? num_1
+		: num_2 > num_1 && num_2 > num_3
+		? num_2
+		: num_3;
+}
+/* console.log(max_number()); */
+
+let result = function (a, b, action) {
+	switch (action) {
+		case "+":
+			return a + b;
+		case "-":
+			return a - b;
+		case "*":
+			return a * b;
+		case "/":
+			return b == 0 ? "На ноль делить нельзя!" : a / b;
+		default:
+			return "Нет такого знака!";
+	}
+};
+console.log(result(1, 0, "/"));
