bdff9c794f6e80d57eb7a4998dfb6a67bfb159fe
Itshnic72 <rv920490@gmail.com>
2024-10-08 12:22:14 +0500

    less_1

---
+++ less_one.js
@@ -0,0 +1,8 @@
+let t = 10;
+let f = (9 / 5) * t + 32;
+console.log("Температура в С", t);
+console.log("Температура в F", f);
+
+let name = "Роман";
+let admin = name;
+console.log("Администратор", admin);
