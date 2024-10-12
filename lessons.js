7c62009f083a30de2818367d1be3d2c3a99ffefd
Roman <rv920490@gmail.com>
2024-10-12 18:14:33 +0500

    less_finish

--- lessons.js
+++ lessons.js
@@ -72,3 +72,106 @@ let result = function (a, b, action) {
 	}
 };
 console.log(result(1, 0, "/"));
+
+console.log("Less - Finish");
+
+/* №-1 */
+
+const numbers = {
+	keyin1: 1,
+	keyin2: 2,
+	keyin3: 3,
+	keyin4: 4,
+	keyin5: 5,
+	keyin6: 6,
+	keyin7: 7,
+};
+
+Object.values(numbers).forEach(item => {
+	if (item >= 3) console.log(item);
+});
+
+/* №-2 */
+const post = {
+	author: "John", // вывести этот текст
+	postId: 23,
+	comments: [
+		{
+			userId: 10,
+			userName: "Alex",
+			text: "lorem ipsum",
+			rating: {
+				likes: 10,
+				dislikes: 2, // вывести это число
+			},
+		},
+		{
+			userId: 5, // вывести это число
+			userName: "Jane",
+			text: "lorem ipsum 2", // вывести этот текст
+			rating: {
+				likes: 3,
+				dislikes: 1,
+			},
+		},
+	],
+};
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
+
+/* №-3 */
+
+const products = [
+	{
+		id: 3,
+		price: 200,
+	},
+	{
+		id: 4,
+		price: 900,
+	},
+	{
+		id: 1,
+		price: 1000,
+	},
+];
+
+products.forEach(element => {
+	element.price -= element.price * 0.15;
+});
+console.log(products);
+
+/* №-4 */
+const product = [
+	{
+		id: 3,
+		price: 127,
+		photos: ["1.jpg", "2.jpg"],
+	},
+	{
+		id: 5,
+		price: 499,
+		photos: [],
+	},
+	{
+		id: 10,
+		price: 26,
+		photos: ["3.jpg"],
+	},
+	{
+		id: 8,
+		price: 78,
+	},
+];
+
+console.log(
+	product.filter(i => {
+		if (i.photos && i.photos.length > 0) return i;
+	})
+);
+
+const arr = product.slice().sort((a, b) => a.price - b.price);
+console.log(arr);
