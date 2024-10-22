30e89e6455cbdb1ee7ed5e7a8d663904f3a6b88f
Itshnic72 <rv920490@gmail.com>
2024-10-22 16:58:40 +0500

    less_2

--- lesson_3.js
+++ lesson_3.js
@@ -14,5 +14,11 @@ const menuEl = document.querySelector(".menu");
 menuEl.classList.remove("dropdown-menu");
 /* 4 */
 const divEl = document.querySelector(".dropdown");
-const tegA = d;
-divEl.insertAdjacentHTML(afterEnd, tegA);
+/* const tegA = document.createElement("a");
+tegA.setAttribute("link", "#"); */
+/* divEl.insertAdjacentHTML("afterEnd", tegA); */
+divEl.insertAdjacentHTML("afterEnd", '<a href="#">link</a>');
+/* 5 */
+const idEl = document.querySelector("#dropdownMenuButton");
+idEl.setAttribute("id", "superDropdown");
+/* 6 */
