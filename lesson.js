dd0141d05989992ef51e76132640649b30cd1677
Roman <rv920490@gmail.com>
2024-11-28 23:35:12 +0500

    less_AJAX

--- lesson.js
+++ lesson.js
@@ -1,74 +1,52 @@
 "use strict";
-console.log("Less - 1");
-let t = 10;
-let f = (9 / 5) * t + 32;
-console.log("Температура в С", t);
-console.log("Температура в F", f);
+/* task - 1 */
+const getUserData = (id, url) => {
+	fetch(url)
+		.then(response => {
+			console.log(response);
+			const arr = response.json();
+			console.log(arr);
+			return arr;
+		})
+		.then(arrUsers => {
+			let userId = arrUsers.filter(user => {
+				if (user.id == id) return user;
+			})[0];
+			console.log(userId);
 
-let name = "Роман";
-let admin = name;
-console.log("Администратор", admin);
-
-console.log("Less - 2");
-/* let num1 = parseFloat(prompt("Введите значение для num1:"));
-let num2 = parseFloat(prompt("Введите значение для num2:"));
-
-if (num1 <= 1 && num2 >= 3) {
-	console.log("Условия выполнены");
-} else {
-	console.log("Условия не выполнены");
-} */
-
-let test = true;
-console.log(test == true ? "+++" : "---");
-
-let day = 31;
-
-let period = day <= 10 ? "первая" : day > 10 && day <= 20 ? "вторая" : "третья";
-console.log(period);
-
-console.log("Less - 6");
-
-function pow(a) {
-	return a ** 3;
-}
-console.log(pow(3));
-
-function ndfl(summ) {
-	if (isNaN(summ)) return "Значение введено не верно!";
-	else
-		return `Размер заработной платы за вычетом налогов равен:${
-			summ - summ * 0.13
-		}р.`;
-}
-console.log(ndfl(100000));
-console.log(ndfl("Привет"));
-
-function max_number() {
-	let num_1 = parseFloat(prompt("Введите первое число:"));
-	let num_2 = parseFloat(prompt("Введите второе число:"));
-	let num_3 = parseFloat(prompt("Введите третье число:"));
-
-	return num_1 > num_2 && num_1 > num_3
-		? num_1
-		: num_2 > num_1 && num_2 > num_3
-		? num_2
-		: num_3;
-}
-/* console.log(max_number()); */
-
-let result = function (a, b, action) {
-	switch (action) {
-		case "+":
-			return a + b;
-		case "-":
-			return a - b;
-		case "*":
-			return a * b;
-		case "/":
-			return b == 0 ? "На ноль делить нельзя!" : a / b;
-		default:
-			return "Нет такого знака!";
-	}
+			if (userId) {
+				console.log(`prise: ${userId.price}`);
+			} else {
+				throw new Error(`Нет такого ${id}`);
+			}
+		})
+		.catch(error => console.error(error));
 };
-console.log(result(1, 0, "/"));
+
+getUserData("0012", "data.json");
+
+/* task - 2 */
+const saveUserData = (urlServer, urlUser) => {
+	const headPost = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json;charset=utf-8",
+		},
+		body: JSON.stringify(urlUser),
+	};
+
+	fetch(urlServer, headPost)
+		.then(response => {
+			if (response.ok) console.log("User data saved successfully");
+			else throw new Error("Failed to save user data");
+		})
+		.catch(error => console.error(error));
+};
+
+/* task - 3 */
+
+const changeStyleDelayed = id => {
+	const el = document.querySelector(`#${id}`);
+	el.style.color = "black";
+};
+setTimeout(changeStyleDelayed, 1000, 112);
