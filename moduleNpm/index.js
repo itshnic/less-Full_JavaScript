798fb2885075220201a774dd463d21aaf5335e42
Itshnic72 <rv920490@gmail.com>
2024-12-11 15:01:17 +0500

    update

--- moduleNpm/index.js
+++ moduleNpm/index.js
@@ -1,5 +1,5 @@
 "use strict";
-function summ(...numbers) {
+function summa(...numbers) {
 	let result = 0;
 	if (!numbers.length) console.log("Аргументы не заданы!!!");
 	else if (numbers.length == 1) result = num[0];
@@ -34,15 +34,18 @@ function multiplication(...numbers) {
 	}
 	return result;
 }
-function division(...numbers) {
+function numberInDegree(number, degree) {
 	let result = 0;
-	if (!numbers.length) console.log("Аргументы не заданы!!!");
-	else if (numbers.length == 1) result = num[0];
+	if (degree == 0) result = 1;
+	else if (degree == 1) result = number;
 	else {
-		result = numbers[0];
-		for (let index = 1; index < numbers.length; index++) {
-			result *= numbers[index];
-		}
+		result = number * number;
 	}
 	return result;
 }
+module.exports = {
+	summa,
+	difference,
+	multiplication,
+	numberInDegree,
+};
---
+++ testNpm/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* const exp = require("exports"); */
+const linkMath = require("../moduleNpm/index.js");
+
+console.log(linkMath.numberInDegree(2, 3));
+console.log(linkMath.multiplication(2, 3, 5));
+console.log(linkMath.difference(5, 3));
+console.log(linkMath.summa(2, 3, 5));
