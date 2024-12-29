ab861ade9e9cfe3f26440454be155472f90be0b6
Roman <rv920490@gmail.com>
2024-12-29 17:18:41 +0500

    test_express

--- express__less/index.js
+++ express__less/index.js
@@ -1,5 +1,42 @@
 "use strict";
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
+
+const data = {
+	countOpenPageHome: 1,
+	countOpenPageAbout: 1,
+};
+
+const pathToFile = path.join(__dirname, "data.json");
+
+fs.writeFileSync(pathToFile, JSON.stringify(data, null, 4), err => {
+	if (err) console.log(err);
+	else console.log("фаил записан");
+});
+
 app.use(express.static("static"));
+app.get("/", (res, req, next) => {
+	/* 	const fileData = JSON.parse(
+		fs.readFileSync(pathToFile, "utf-8", (err, data) => {
+			if (err) console.log(err);
+			else return data;
+		})
+	); */
+	res.send(`<span>2</span>`);
+	/* 	fs.writeFileSync(pathToFile, JSON.stringify(fileData, null, 4));
+	next(); */
+});
+app.get("/about", (res, req) => {
+	/* 	const fileData = JSON.parse(
+		fs.readFileSync(pathToFile, "utf-8", (err, data) => {
+			if (err) console.log(err);
+			else return data;
+		})
+	); */
+	res.send(`<span>1</span>`);
+	/* fs.writeFileSync(pathToFile, JSON.stringify(fileData, null, 4)); */
+});
 app.listen(3000);
