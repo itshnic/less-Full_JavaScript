502aaa42d478e2b676818058b2e153d2c0414bf6
Itshnic72 <rv920490@gmail.com>
2025-01-22 13:02:20 +0500

    update_less_Rest_API

--- rest_api/index.js
+++ rest_api/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const fs = require("fs");
+const Joi = require("joi");
 
 const app = express();
 
 app.use(express.json());
 
 const fileName = "data.txt";
+
+const schemeValidData = Joi.object({
+	name: Joi.string().min(3).required(),
+	city: Joi.string().min(3).required(),
+});
+const schemeValidId = Joi.object({
+	id: Joi.number().min(1).required(),
+});
 
 if (!fs.existsSync(fileName)) {
 	writeDataUsers(fileName, []);
@@ -33,12 +42,40 @@ app.get("/users", (req, res) => {
 });
 
 app.get("/users/:id", (req, res) => {
+	const validId = schemeValidId.validate(req.params);
+	if (validId.error) {
+		console.log(`В url не корректные данные ${validId.error.details}`);
+		return res
+			.status(400)
+			.send(
+				`В url не корректные данные ${JSON.stringify(
+					validId.error.details[0].context,
+					null,
+					4
+				)}`
+			);
+	}
+
 	const userId = +req.params.id;
 	const user = users.find(user => user.id === userId);
 	user ? res.send({ user }) : res.send(`Пользователь id = ${userId} не найден`);
 });
 
 app.post("/users", (req, res) => {
+	const validData = schemeValidData.validate(req.body);
+	if (validData.error) {
+		console.log(validData.error.details);
+		return res
+			.status(400)
+			.send(
+				`Введены не корректные данные ${JSON.stringify(
+					validData.error.details[0].context,
+					null,
+					4
+				)}`
+			);
+	}
+
 	let uniqueId = 1;
 	if (users.length > 0) {
 		uniqueId = users[users.length - 1].id + 1;
@@ -52,6 +89,26 @@ app.post("/users", (req, res) => {
 });
 
 app.put("/users/:id", (req, res) => {
+	const validId = schemeValidId.validate(req.params);
+	if (validId.error) {
+		console.log(
+			`В url не корректные данные ${JSON.stringify(
+				validId.error.details[0].context,
+				null,
+				4
+			)}`
+		);
+		return res
+			.status(400)
+			.send(
+				`В url не корректные данные ${JSON.stringify(
+					validId.error.details[0].context,
+					null,
+					4
+				)}`
+			);
+	}
+
 	const userId = +req.params.id;
 	const user = users.find(user => user.id === userId);
 	if (user) {
@@ -64,6 +121,26 @@ app.put("/users/:id", (req, res) => {
 });
 
 app.delete("/users/:id", (req, res) => {
+	const validId = schemeValidId.validate(req.params);
+	if (validId.error) {
+		console.log(
+			`В url не корректные данные ${JSON.stringify(
+				validId.error.details[0].context,
+				null,
+				4
+			)}`
+		);
+		return res
+			.status(400)
+			.send(
+				`В url не корректные данные ${JSON.stringify(
+					validId.error.details[0].context,
+					null,
+					4
+				)}`
+			);
+	}
+
 	const userId = +req.params.id;
 	const user = users.find(user => user.id === userId);
 	if (user) {
