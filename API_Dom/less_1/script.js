e60154e267966e91a46b355980fd765e1757c3ee
Itshnic72 <rv920490@gmail.com>
2025-01-27 15:21:53 +0500

    less DOM

--- API_Dom/less_1/script.js
+++ API_Dom/less_1/script.js
@@ -23,31 +23,27 @@ function readFile(path) {
 	});
 }
 
-arrayData = readFile(path);
-console.log(arrayData);
-
-const lessonsBlock = document.querySelector(".lessons");
-
-if (arrayData) {
-	arrayData.forEach(el => {
-		const lessonsItem = document.createElement("li");
-		lessonsItem.classList.add = "lessons__item";
-		lessonsItem.textContent = `<p class="lessons__item-name">${el.name}</p>
-					<p class="lessons__item-date">
-						Дата начала занятия: <span>${el.date}</span>
-					</p>
-					<p class="lessons__item-time">
-						Время начала занятия: <span>${el.time}</span>
-					</p>
-					<p class="countPeople_max">
-						Максимальное количество учеников: <span>${el.maxCountPeoples}</span>
-					</p>
-					<p class="countPeople_registered">
-						Зарегистрировалось учеников: <span class="_red">${el.registrationCountPeoples}</span>
-					</p>
-					<p class="lessons__item-textInfo _red">Мест нет!</p>
-					<button class="lessons__item-btn">Запись на занятие</button>
+/* function renderHtml(arrayData, classNameParent) {
+	if (arrayData) {
+		arrayData.forEach(el => {
+			const lessonsItem = document.createElement("li");
+			lessonsItem.classList.add = "lessons__item";
+			lessonsItem.textContent = `
 		`;
-		lessonsBlock.appendChild(lessonsItem);
-	});
-}
+			parentBlock.appendChild(lessonsItem);
+		});
+	} else console.log("Массив пуст!");
+} */
+
+app.get("/", (req, res) => {
+	arrayData = readFile(path);
+	res.sendFile("static/index.html");
+});
+
+app.get("/", (req, res) => {
+	console.log(arrayData);
+	res.send(renderHtml(arrayData, "lessons"));
+});
+app.listen(3000, () => {
+	console.log("Сервер запущен на порту 3000)");
+});
