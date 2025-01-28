6458bbd0d0c4cfcf6c12879a069fb4913d144217
Itshnic72 <rv920490@gmail.com>
2025-01-28 12:20:26 +0500

    update less Dom

--- API_Dom/less_1/static/index.js
+++ API_Dom/less_1/static/index.js
@@ -2,15 +2,55 @@
 const btn = document.querySelectorAll(".lessons__item-btn");
 console.log(btn);
 
+function changeQuantityPeople(element) {
+	const parentBlock = element.closest(".lessons__item");
+	const countPeopleMaxBlock = parentBlock
+		.querySelector(".countPeople_max")
+		.querySelector("span");
+	const countPeopleRegisteredBlock = parentBlock
+		.querySelector(".countPeople_registered")
+		.querySelector("span");
+	const textInfo = parentBlock.querySelector(".lessons__item-textInfo");
+	const idLesson = parentBlock.id;
+	let countPeopleRegistered = +countPeopleRegisteredBlock.textContent;
+
+	if (+countPeopleMaxBlock.textContent > countPeopleRegistered) {
+		countPeopleRegistered += 1;
+		countPeopleRegisteredBlock.textContent = countPeopleRegistered;
+	} else {
+		if (!countPeopleRegisteredBlock.classList.contains("_red"))
+			countPeopleRegisteredBlock.classList.add("_red");
+		if (!textInfo.classList.contains("_red")) {
+			textInfo.classList.add("_red");
+		}
+		if (textInfo.classList.contains("_hide")) {
+			textInfo.classList.remove("_hide");
+		}
+	}
+
+	return { id: idLesson, countPeople: countPeopleRegistered };
+}
+
 btn.forEach(el => {
-	el.addEventListener("click", e => {});
+	el.addEventListener("click", e => {
+		const dataPeople = changeQuantityPeople(e.target);
+
+		fetch("http://localhost:3000/update", {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ key: dataPeople }),
+		})
+			.then(res => res.json())
+			.then(data => console.log("Успешно:", data))
+			.catch(error => console.error("Ошибка:", error));
+	});
 });
 
-fetch("your-endpoint", {
+/* fetch("your-endpoint", {
 	method: "POST",
 	headers: { "Content-Type": "application/json" },
-	body: JSON.stringify({ key: "value" }), // Ваши данные для отправки
+	body: JSON.stringify(dataPeople), // Ваши данные для отправки
 })
 	.then(res => res.json())
 	.then(data => console.log("Успешно:", data))
-	.catch(error => console.error("Ошибка:", error));
+	.catch(error => console.error("Ошибка:", error)); */
