c6916990386cc2a525019acde2e0f6353b5fea74
Itshnic72 <rv920490@gmail.com>
2025-02-11 14:28:53 +0500

    update less_3

--- API_Dom/less_3/script.js
+++ API_Dom/less_3/script.js
@@ -1,26 +1,28 @@
 "use strict";
+const url =
+	"https://api.unsplash.com/photos/?client_id=pIePwdcizPXl7cPzexlv07APRmFfZrghd2_udjV9-EM";
+let ArrayData = [];
 
 /* fetch(
 	"https://api.unsplash.com/photos/?client_id=pIePwdcizPXl7cPzexlv07APRmFfZrghd2_udjV9-EM"
 )
 	.then(response => response.json())
-	.then(result => console.log(result)); */
+	.then(result => {
+		result.forEach(element => {
+			ArrayData.push(element);
+		});
+	}); */
 
-const getData = async () => {
-	const data = await fetch(
+/* const data = function (url) {
+	return fetch(
 		"https://api.unsplash.com/photos/?client_id=pIePwdcizPXl7cPzexlv07APRmFfZrghd2_udjV9-EM"
-	);
-
-	if (data.ok) {
-		return await data.json();
-	} else {
-		throw new Error(
-			`Данные не были получены, ошибка ${data.status} ${data.statusText}`
-		);
-	}
+	).then(response => response.json());
 };
-async function arrayData() {
-	return await getData();
-}
-const data = arrayData();
-console.log(data);
+
+console.log(data(url).then(response => response)); */
+const app = async () => {
+	const obj = await fetch(url);
+	const result = await obj.json();
+};
+
+app();
