8a3515a847cd72eedadc64110b9ceda4680b97b4
Itshnic72 <rv920490@gmail.com>
2025-02-13 17:03:16 +0500

    update less_3

--- API_Dom/less_3/script.js
+++ API_Dom/less_3/script.js
@@ -1,28 +1,45 @@
 "use strict";
 const url =
 	"https://api.unsplash.com/photos/?client_id=pIePwdcizPXl7cPzexlv07APRmFfZrghd2_udjV9-EM";
-let ArrayData = [];
 
-/* fetch(
-	"https://api.unsplash.com/photos/?client_id=pIePwdcizPXl7cPzexlv07APRmFfZrghd2_udjV9-EM"
-)
-	.then(response => response.json())
-	.then(result => {
-		result.forEach(element => {
-			ArrayData.push(element);
+const data = function (url) {
+	return fetch(url)
+		.then(response => response.json())
+		.then(result => {
+			console.log(result);
+			renderHtml(result);
+			clickForImg(document.querySelectorAll(".list__item"));
 		});
-	}); */
-
-/* const data = function (url) {
-	return fetch(
-		"https://api.unsplash.com/photos/?client_id=pIePwdcizPXl7cPzexlv07APRmFfZrghd2_udjV9-EM"
-	).then(response => response.json());
 };
 
-console.log(data(url).then(response => response)); */
-const app = async () => {
-	const obj = await fetch(url);
-	const result = await obj.json();
-};
+const renderHtml = array => {
+	const body = document.querySelector("body");
+	const list = document.createElement("ul");
+	list.classList.add("list__photo");
+	body.prepend(list);
+	let item = null;
+	let img = null;
+	let countLike = null;
 
-app();
+	array.forEach(linkForImg => {
+		item = document.createElement("li");
+		img = document.createElement("img");
+		countLike = document.createElement("span");
+
+		item.classList.add("list__item");
+		img.src = linkForImg.urls.small;
+
+		list.prepend(item);
+		item.prepend(img);
+		item.prepend(countLike);
+	});
+};
+const clickForImg = array => {
+	array.forEach(el => {
+		el.addEventListener("click", e => {
+			const boxItem = e.target.offsetParent;
+			boxItem.querySelector("span").textContent++;
+		});
+	});
+};
+data(url);
