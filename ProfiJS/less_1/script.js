bef1a3eed3337cbf0fa6c67f2a6640231ee56474
Roman <rv920490@gmail.com>
2025-02-16 23:48:36 +0500

    profi_js less_1

---
+++ ProfiJS/less_1/script.js
@@ -0,0 +1,15 @@
+"use strict";
+let pseudo = {
+	id: "name",
+	gh: "last-name",
+	length: 2,
+};
+let array = Array.from(pseudo);
+
+console.log(array);
+
+for (const element of array) {
+	console.log(element);
+}
+/* array.pop();
+console.log(array); */
