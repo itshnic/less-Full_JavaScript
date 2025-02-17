9fe135fc3ccf0ce0022b94cb9234d55dc3b5f057
Itshnic72 <rv920490@gmail.com>
2025-02-17 17:12:00 +0500

    less_1

--- ProfiJS/less_1/script.js
+++ ProfiJS/less_1/script.js
@@ -1,15 +1,40 @@
 "use strict";
-let pseudo = {
-	id: "name",
-	gh: "last-name",
-	length: 2,
+/* Less_1-1 */
+const musicCollection = [
+	{ title: "Осень", artist: "Чайф", year: "2009" },
+	{ title: "Зима", artist: "Звери", year: "2015" },
+	{ title: "Лето", artist: "Asti", year: "2018" },
+	{ title: "Весна", artist: "Елка", year: "2020" },
+];
+
+let iterator = objIterator => {
+	objIterator[Symbol.iterator] = function () {
+		return {
+			index: 0,
+			values: Object.values(this),
+			next() {
+				return this.index < this.values.length
+					? { done: false, value: this.values[this.index++] }
+					: { done: true };
+			},
+		};
+	};
 };
-let array = Array.from(pseudo);
 
-console.log(array);
-
-for (const element of array) {
-	console.log(element);
+for (const album of musicCollection) {
+	iterator(album);
+	console.log("");
+	for (const el of album) {
+		console.log(el);
+	}
 }
-/* array.pop();
-console.log(array); */
+
+/* Less_1-2 */
+
+const cook = ["Виктор", "Ольга", "Дмитрий"];
+const dishes = {
+	Pizza: ["Маргарита", "Пепперони"],
+	Sushi: ["Филадельфия", "Калифорния"],
+	dessert: ["Тирамису ", "Чизкейк"],
+};
+const client = ["Алексей ", "Мария ", "Ирина "];
